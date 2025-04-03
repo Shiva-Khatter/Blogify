23a19812e1ea9323fd561cb8d2eaa8649c719528
Shiva Khatter <shiva.khatter@gmail.com>
2025-04-03 11:00:14 +0530

    Updating the settings.py, media locations and removing bash file

--- django_project/settings.py
+++ django_project/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [BASE_DIR / "blog/static"]
+STATICFILES_DIRS = []
 
 MEDIA_ROOT = Path(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
