07f9d727a00223131c704e9270c9801428df848f
Shiva Khatter <shiva.khatter@gmail.com>
2025-04-22 11:43:52 +0530

    Updating celery.py for local scheduling

--- django_project/celery.py
+++ django_project/celery.py
@@ -7,9 +7,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
 
 app = Celery('django_project')
 
-# Using a string here means the worker doesn't have to serialize
+# Using a string here means the worker doesn't have to serialize 
 # the configuration object to child processes.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Load task modules from all registered Django app configs
+#Loading task modules from all registered Django app Configs 
 app.autodiscover_tasks()
