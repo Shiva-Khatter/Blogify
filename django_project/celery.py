b881fe0bcacb2970651d89e87c39df112e56507c
Shiva Khatter <shiva.khatter@gmail.com>
2025-04-24 13:20:40 +0530

    Updating celery.py

--- django_project/celery.py
+++ django_project/celery.py
@@ -2,7 +2,7 @@
 import os
 from celery import Celery
 
-# Set the default Django settings module for the 'celery' program
+# Setting the default Django settings module for the 'celery' program
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
 
 app = Celery('django_project')
